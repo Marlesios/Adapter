6871109dbdc69ba178831d9eddc8a5f88fd86ea7
Marlesios <marles.nuralyev@gmail.com>
2022-08-05 00:41:01 +0300

    fisished project

--- src/main/java/rock/adapter/IntsCalculator.java
+++ src/main/java/rock/adapter/IntsCalculator.java
@@ -1,24 +1,46 @@
 package rock.adapter;
 
+import java.security.PublicKey;
 import java.text.Normalizer;
 
-public class IntsCalculator implements Ints {
+public class IntsCalculator  implements Ints {
     protected final Calculator target;
-
     public IntsCalculator() { this.target = new Calculator(); }
 
     @Override
     public int sum(int arg0, int arg1) {
-        return arg0 + arg1;
+        return (int) target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.SUM)
+                .result();
     }
 
     @Override
     public int mult(int arg0, int arg1) {
+        return (int) target.newFormula()
+                     .addOperand(arg0)
+                     .addOperand(arg1)
+                     .calculate(Calculator.Operation.MULT)
+                     .result();
 
     }
 
     @Override
     public int pow(int a, int b) {
-      return (int) Math.pow(a,b);
+        return (int) target.newFormula()
+                .addOperand(a)
+                .addOperand(b)
+                .calculate(Calculator.Operation.POW)
+                .result();
+    }
+
+    @Override
+    public int div(int a, int b) {
+        return (int) target.newFormula()
+                .addOperand(a)
+                .addOperand(b)
+                .calculate(Calculator.Operation.DIV)
+                .result();
     }
 }
--- src/main/java/rock/adapter/Main.java
+++ src/main/java/rock/adapter/Main.java
@@ -4,18 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Calculator calc = new Calculator();
-        System.out.println(
-                calc.newFormula()
-                        .addOperand(5)
-                        .addOperand(15)
-                        .calculate(Calculator.Operation.MULT)
-                        .result()
-        );
-
         Ints intsCalc = new IntsCalculator();
-        System.out.println(intsCalc.sum(2, 2));
-        System.out.println(intsCalc.sum(10, 22) + " сделанный метод");
-        System.out.println(intsCalc.pow(2, 10));
+        System.out.println("результат деления: " + intsCalc.sum(2, 2));
+        System.out.println("результат умножения: " + intsCalc.mult(23,10));
+        System.out.println("результат деления: "+ intsCalc.div(100,5));
+        System.out.println("результат экспоненции: " + intsCalc.pow(2, 10));
     }
+
 }
